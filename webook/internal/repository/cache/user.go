72a177a7bb349c98fade2e3a867453cb85f6c0f7
lihaonan <lhn199699uow@gmail.com>
2024-10-09 15:59:00 +0800

    项目初始化。

---
+++ webook/internal/repository/cache/user.go
@@ -0,0 +1,61 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/ClearloveHn/webook/webook/internal/domain"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+var ErrKeyNotExist = redis.Nil
+
+type UserCache interface {
+	Get(ctx context.Context, uid int64) (domain.User, error)
+	Set(ctx context.Context, du domain.User) error
+}
+
+type RedisUserCache struct {
+	cmd        redis.Cmdable
+	expiration time.Duration
+}
+
+func NewUserCache(cmd redis.Cmdable) UserCache {
+	return &RedisUserCache{
+		cmd:        cmd,
+		expiration: time.Minute * 15,
+	}
+}
+
+func (c *RedisUserCache) Get(ctx context.Context, uid int64) (domain.User, error) {
+	key := c.key(uid)
+	data, err := c.cmd.Get(ctx, key).Result()
+	if err != nil {
+		return domain.User{}, err
+	}
+	var u domain.User
+	err = json.Unmarshal([]byte(data), &u)
+	return u, err
+}
+
+func (c *RedisUserCache) Set(ctx context.Context, du domain.User) error {
+	key := c.key(du.Id)
+	data, err := json.Marshal(du)
+	if err != nil {
+		return err
+	}
+	return c.cmd.Set(ctx, key, data, c.expiration).Err()
+}
+
+func (c *RedisUserCache) key(uid int64) string {
+	// user-info-
+	// user.info.
+	// user/info/
+	// user_info_
+	return fmt.Sprintf("user:info:%d", uid)
+}
+
+type UserCacheV1 struct {
+	client *redis.Client
+}
